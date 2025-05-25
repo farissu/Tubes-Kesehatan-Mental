5f312431175856fe0c2dfa310690944105b55632
faris <rizkifaris2207@gmail.com>
2025-05-25 19:10:45 +0700

    feat: add menu function for mental health management app

--- main.go
+++ main.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+func menu(pilihan int) {
+	fmt.Println("---------------------------------------------------")
+	fmt.Println("        Aplikasi Manajemen Kesehatan Mental        ")
+	fmt.Println("---------------------------------------------------")
+	fmt.Println("Silakan pilih opsi di bawah ini:")
+	fmt.Println("1. Isi Kuesioner")
+	fmt.Println("2. Edit Kuesioner")
+	fmt.Println("3. Hapus Kuesioner")
+	fmt.Println("4. Cari Kuesioner")
+	fmt.Println("5. Cek Laporan")
+	fmt.Println("6. Keluar")
+	fmt.Println("---------------------------------------------------")
+	fmt.Print("Pilih opsi (1-6): ")
+	fmt.Scanln(&pilihan)
+
+}
 func main() {
-	fmt.Println("Hello World")
+	var pilihan int
+	menu(pilihan)
 }
